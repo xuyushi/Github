c8da88e9543b696b2a939b9ae4afa9f2e8c496e1
xuyushi <xmy166@gmail.com>
2015-02-21 12:35:41 +0800

    test1

--- app/src/main/java/com/example/xuyushi/github/MainActivity.java
+++ app/src/main/java/com/example/xuyushi/github/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test1
+        //branch 1
     }
 
 
